4d6cd21f0b5972e4d6aabcf0fee8ad6c2964eae5
ClarissaCardarelli <ccardare@gmail.com>
2025-11-17 12:11:18 +0100

    Quiz instructions page header created with variable to toggle to quiz itself

--- src/pages/Quiz/Quiz.tsx
+++ src/pages/Quiz/Quiz.tsx
@@ -1,5 +1,34 @@
-function Quiz() {
-	return true;
-}
+import { useState } from "react";
+import "./Quiz.css";
 
-export default Quiz;
+export default function Quiz() {
+	const [quizStarted, setQuizStarted] = useState(false);
+
+	return (
+		<>
+			<div className="quiz-bg">
+				{quizStarted ? (
+					<div>QUIZ HERE</div>
+				) : (
+					<section className="header-section-center quiz-hero">
+						<h1 className="primary-title">
+							Découvre des films selon ton
+							<span className="body-text-blue"> humeur</span> !
+						</h1>
+						<p className="body-text">
+							<span className="body-text-bold">Sans réfléchir</span>, clique
+							aussi vite que possible sur l'image que tu préfères sur le moment.
+						</p>
+						<button
+							type="button"
+							className="primary-button low-emphasis-button"
+							onClick={() => setQuizStarted(true)}
+						>
+							Lance le quiz
+						</button>
+					</section>
+				)}
+			</div>
+		</>
+	);
+}
