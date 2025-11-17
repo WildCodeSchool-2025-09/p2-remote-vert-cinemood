9cc7793ce1845028644a5709a89a91be2f7cbc45
Kevin Ressegaire <176156975+earzalien@users.noreply.github.com>
2025-11-17 19:56:55 +0100

    fonction pour créer les questions et affichage des images dans les boutons

--- src/components/Quiz/Quiz.tsx
+++ src/components/Quiz/Quiz.tsx
@@ -2,37 +2,53 @@ import { useState } from "react";
 import "./Quiz.css";
 import quizPictures from "./genresImage";
 
-function Quizz () {
-  const [buttons] = useState([quizPictures[0], quizPictures[1]]);
+function createImageQuestions(imagesData) {
+	const imageQuestions = [];
+	for (let i = 0; i < imagesData.length; i++) {
+		for (let j = i + 1; j < imagesData.length; j++) {
+			imageQuestions.push({
+				questionId: imageQuestions.length + 1,
+				imageA: imagesData[i],
+				imageB: imagesData[j],
+			});
+		}
+	}
+	return imageQuestions;
+}
 
-return (
-	<>
-		<section className="primary-background">
-			<div className="quiz-container">
-				<h1 className="primary-title">
-					Laisse ton humeur <span className="body-text-blue">te guider</span>
-					<br />
-					vers le bon film
-				</h1>
-				<div className="questions">
-					{buttons.map((images, id) => (
+function Quizz() {
+	const [questionNumber, setQuestioNumber] = useState(0);
+	const currentQuestion = createImageQuestions(quizPictures)[questionNumber];
+
+	return (
+		<>
+			<section className="primary-background">
+				<div className="quiz-container">
+					<h1 className="primary-title">
+						Laisse ton humeur <span className="body-text-blue">te guider</span>
+						<br />
+						vers le bon film
+					</h1>
+					<div className="questions">
 						<button
 							type="button"
-							key={images.id}
 							className="quiz-images"
 							style={{
-								backgroundImage: `url(../../assets/images/quizImages/${images.id}.jpg)`,
-								backgroundSize: "cover",
-								backgroundPosition: "center",
+								backgroundImage: `url(/quizImages/${currentQuestion.imageA.id}.jpg)`,
 							}}
-							aria-label={`Image quiz ${id + 1}`}
 						/>
-					))}
+						<button
+							type="button"
+							className="quiz-images"
+							style={{
+								backgroundImage: `url(/quizImages/${currentQuestion.imageB.id}.jpg)`,
+							}}
+						/>
+					</div>
 				</div>
-			</div>
-		</section>
-	</>
-);
+			</section>
+		</>
+	);
 }
 
 export default Quizz;
