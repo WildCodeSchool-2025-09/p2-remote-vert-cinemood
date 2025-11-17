628176c1d3566d75087cdd496344b1a3cc95f530
Kevin Ressegaire <176156975+earzalien@users.noreply.github.com>
2025-11-17 14:29:37 +0100

    us07

--- src/pages/Quiz/Quiz.tsx
+++ src/pages/Quiz/Quiz.tsx
@@ -1,10 +1,35 @@
+import { useState } from "react";
+import "./Quiz.css";
 import Quizz from "../../components/Quiz/Quiz";
 
+export default function Quiz() {
+	const [quizStarted, setQuizStarted] = useState(false);
 
-function Quiz() {
 	return (
-		<Quizz />
-	)
+		<>
+			<div className="quiz-bg">
+				{quizStarted ? (
+					<Quizz />
+				) : (
+					<section className="header-section-center quiz-hero">
+						<h1 className="primary-title">
+							Découvre des films selon ton
+							<span className="body-text-blue"> humeur</span> !
+						</h1>
+						<p className="body-text">
+							<span className="body-text-bold">Sans réfléchir</span>, clique
+							aussi vite que possible sur l'image que tu préfères sur le moment.
+						</p>
+						<button
+							type="button"
+							className="primary-button"
+							onClick={() => setQuizStarted(true)}
+						>
+							Lance le quiz
+						</button>
+					</section>
+				)}
+			</div>
+		</>
+	);
 }
-
-export default Quiz;
