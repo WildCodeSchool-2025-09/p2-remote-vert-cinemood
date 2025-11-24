a47a47a5a6691ebd6c8dad3a4909d76345a7efb2
Solomon Kalandadze <solokalandadze@gmail.com>
2025-11-24 10:22:52 +0100

    saving before moving search to navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 import SearchInput from "../SearchInput/SearchInput";
 import "./NavBar.css";
 import { useContext } from "react";
@@ -9,12 +10,24 @@ function NavBar() {
 
 	return (
 		<div className="navbar">
-			<SearchInput
-				isOpen={isOpen}
-				setFilteredMovies={setFilteredMovies}
-				movieData={allMovies}
-				setIsOpen={setIsOpen}
-			/>
+			<img src="/favicon.ico" alt="logo" className="logo" />
+			<div className="links">
+				<Link to="/" className="primary-button" id="apropo-link">
+					À propos
+				</Link>
+				<Link to="/catalogue" id="catalog-link">
+					Catalogue
+				</Link>
+				<SearchInput
+					isOpen={isOpen}
+					setFilteredMovies={setFilteredMovies}
+					movieData={allMovies}
+					setIsOpen={setIsOpen}
+				/>
+				<Link to="/quiz" className="primary-button" id="quiz-link">
+					Lance le Quiz
+				</Link>
+			</div>
 		</div>
 	);
 }
