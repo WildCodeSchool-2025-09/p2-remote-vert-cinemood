0b9417dd0daf091558431e64c60764d57cb2eca8
Kevin Ressegaire <176156975+earzalien@users.noreply.github.com>
2025-11-25 17:18:47 +0100

    footer done

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,54 @@
+import { Link } from "react-router";
+import "./Footer.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
 function Footer() {
-	return true;
+	return (
+		<>
+			<footer className="bas-de-page">
+				<Link to="/">
+					<img src="/logo-cine-mood-black.png" alt="logo" height="80px" />
+				</Link>
+				<p className="body-text footer">
+					EXPLORER
+					<ul>
+						<Link to="/quiz">
+							<li>Quiz</li>
+						</Link>
+						<li>Profil</li>
+						<Link to="/catalogue">
+							<li>Catalogue</li>
+						</Link>
+					</ul>
+				</p>
+				<p className="body-text footer">
+					SOCIÉTÉ
+					<ul>
+						<li>A propos</li>
+						<li>contact</li>
+						<li>Actualités</li>
+					</ul>
+				</p>
+				<p className="body-text footer">
+					LÉGAL
+					<ul>
+						<li>Mentions légales</li>
+						<li>Politique de confidentialité</li>
+						<li>Conditions d'utilisation</li>
+					</ul>
+				</p>
+				<p className="body-text footer">
+					Suivez-nous
+					<br />
+					<span className="follow">
+						<i className="bi bi-twitter-x body-text" />
+						<i className="bi bi-facebook body-text" />
+						<i className="bi bi-instagram body-text" />
+					</span>
+				</p>
+			</footer>
+		</>
+	);
 }
 
 export default Footer;
