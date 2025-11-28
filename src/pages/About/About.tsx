5c0d67a25d567544d000631e0204097b7e72e909
Kevin Ressegaire <176156975+earzalien@users.noreply.github.com>
2025-11-28 15:11:12 +0100

    about page done

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,7 +1,35 @@
+import "./About.css";
+
 function About() {
 	return (
 		<>
-			<h1>A propos</h1>
+			<section className="primary-background about">
+				<article>
+					<h1 className="primary-title">A propos</h1>
+					<p>
+						Bienvenue sur Ciné Mood, notre projet web réalisé par Clarissa,
+						Jeremy, Kevin et Solo au sein de Wild Code School (session 09/25).
+					</p>
+					<p>
+						Ce site est né d'une idée simple : le cinéma est un miroir de nos
+						émotions. Nous avons voulu créer un espace qui relie les films à
+						l'humeur du spectateur, un lieu où chaque émotion trouve son écran.
+					</p>
+					<p>
+						À travers ce travail, nous avons fusionné créativité, design
+						responsive et technologies web pour créer une expérience intuitive
+						et personnalisée.
+					</p>
+					<p>
+						C'est avant tout une aventure collaborative, portée par la passion
+						du cinéma et le plaisir d'apprendre ensemble.
+					</p>
+					<p>
+						Merci de découvrir notre travail et testez comment votre humeur
+						révèle vos films préférés.
+					</p>
+				</article>
+			</section>
 		</>
 	);
 }
