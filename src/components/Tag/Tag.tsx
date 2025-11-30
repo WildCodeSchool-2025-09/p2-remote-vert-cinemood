bc0bc4fc47b053ea8ae89b65109268a803d83ab0
ClarissaCardarelli <ccardare@gmail.com>
2025-11-30 11:57:03 +0100

    component Tag is working with 3 different tags/lists

---
+++ src/components/Tag/Tag.tsx
@@ -0,0 +1,31 @@
+export default function Tag({ list, setter, icon, movie }) {
+	const isActive = list.some((listItem) => listItem.id === movie.id);
+
+	function addtoList(setter, para) {
+		setter((prev) => {
+			const exists = prev.some((listItem) => listItem.id === para.id);
+			if (exists) {
+				return prev.filter((listItem) => listItem.id !== para.id);
+			}
+			return [...prev, movie];
+		});
+	}
+
+	return (
+		<div className="icon">
+			<i
+				className={isActive ? `${icon}-fill tag-on` : icon}
+				onClick={() => {
+					addtoList(setter, movie);
+				}}
+				onKeyDown={(e) => {
+					if (e.key === "Enter") {
+						addtoList(setter, movie);
+					}
+				}}
+				role="button"
+				tabIndex={0}
+			/>
+		</div>
+	);
+}
