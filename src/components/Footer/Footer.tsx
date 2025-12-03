65f876a096c3c180950a55c2994f14afd0b9c311
Kevin Ressegaire <176156975+earzalien@users.noreply.github.com>
2025-12-03 14:16:53 +0100

    footer link

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,17 +6,33 @@ function Footer() {
 	return (
 		<>
 			<footer>
-				<Link to="/">
+				<Link
+					to="/"
+					onClick={() => {
+						window.scrollTo({ top: 0, left: 0 });
+					}}
+				>
 					<img src="/logo-cine-mood-black.png" alt="logo" height="80px" />
 				</Link>
 				<p className="body-text footer">
 					EXPLORER
 					<ul>
-						<Link to="/quiz" className="link">
+						<Link
+							to="/quiz"
+							className="link"
+							onClick={() => {
+								window.scrollTo({ top: 0, left: 0 });
+							}}
+						>
 							<li>Quiz</li>
 						</Link>
 						<li className="link">Profil</li>
-						<Link to="/catalogue">
+						<Link
+							to="/catalogue"
+							onClick={() => {
+								window.scrollTo({ top: 0, left: 0 });
+							}}
+						>
 							<li>Catalogue</li>
 						</Link>
 					</ul>
