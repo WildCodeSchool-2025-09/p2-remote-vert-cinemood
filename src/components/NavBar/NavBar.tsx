06b9d61a087b86f7e59fe6099a5546005f7ff9d3
Kevin Ressegaire <176156975+earzalien@users.noreply.github.com>
2025-12-03 12:20:19 +0100

    merge dev

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -19,8 +19,12 @@ function NavBar() {
 						src="/logo-cine-mood-black.png"
 						alt="CinéMood"
 						className={`logo ${isMenuOpen ? "" : "show"}`}
-						onClick={() => {window.scrollTo({ top: 0, left: 0 });
-}}
+						onClick={() => {
+							window.scrollTo({ top: 0, left: 0 });
+						}}
+						onKeyUp={() => {
+							window.scrollTo({ top: 0, left: 0 });
+						}}
 					/>
 				</Link>
 				<div
@@ -42,10 +46,7 @@ function NavBar() {
 						</Link>
 					</li>
 					<li className="navbar-item">
-						<Link to="/catalogue" className="nav-button" 							
-						onClick={
-								toggleMenu	
-						}>
+						<Link to="/catalogue" className="nav-button" onClick={toggleMenu}>
 							Catalogue
 						</Link>
 					</li>
@@ -60,7 +61,7 @@ function NavBar() {
 							onClick={() => {
 								setLaunch(true);
 								toggleMenu();
-								window.scrollTo({ top: 0, left: 0 });	
+								window.scrollTo({ top: 0, left: 0 });
 							}}
 						>
 							Lance le Quiz
