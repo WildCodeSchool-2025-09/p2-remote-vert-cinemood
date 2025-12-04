d26a7637b1aad986fb9a73d3a9e83139feca0483
ClarissaCardarelli <ccardare@gmail.com>
2025-12-04 10:26:20 +0100

    ajoute tags sur movie card

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -5,58 +5,63 @@ import { useAlreadySeenMovieList } from "../../context/AlreadySeenMovieListConte
 import { useFavoriteMoviesList } from "../../context/FavoriteMovieListContext";
 import { useWatchListMovies } from "../../context/WatchListMoviesContext";
 import Tag from "../Tag/Tag";
+import type { MovieData } from "../../types/MovieType";
 
-function MovieCard({ movie }) {
-	const imageUrl = movie.poster_path
-		? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
-		: "https://via.placeholder.com/500x750?text=No+Image";
-	const { AlreadySeenMovieList, setAlreadySeenMovieList } =
-		useAlreadySeenMovieList();
-	const { FavoriteMoviesList, setFavoriteMoviesList } = useFavoriteMoviesList();
-	const { WatchListMovies, setWatchListMovies } = useWatchListMovies();
+type MovieCardProps = {
+  movie: MovieData;
+};
 
-	return (
-		<>
-			<div className="movie-card-wraper">
-				<div className="movie-card">
-					<img src={imageUrl} alt={movie.title} className="movie-image" />
+function MovieCard({ movie }: MovieCardProps) {
+  const imageUrl = movie.poster_path
+    ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+    : "https://via.placeholder.com/500x750?text=No+Image";
+  const { AlreadySeenMovieList, setAlreadySeenMovieList } =
+    useAlreadySeenMovieList();
+  const { FavoriteMoviesList, setFavoriteMoviesList } = useFavoriteMoviesList();
+  const { WatchListMovies, setWatchListMovies } = useWatchListMovies();
 
-					<div className="card-content">
-						{/* <h3>{movie.title}</h3> */}
-						<Link to={`/film/${movie.id}`} className="plus-dinfo-btn">
-							Plus d'info
-						</Link>
-						<div className="show-mini-details">
-							<p className="title-small-carousel">{movie.title}</p>
-							<div className="tag-list-carousel">
-								<Tag
-									className="icon-small-carousel"
-									list={FavoriteMoviesList}
-									setter={setFavoriteMoviesList}
-									icon="bi bi-suit-heart"
-									movie={movie}
-								/>
-								<Tag
-									className="icon-small-carousel"
-									list={WatchListMovies}
-									setter={setWatchListMovies}
-									icon="bi bi-plus-circle"
-									movie={movie}
-								/>
-								<Tag
-									className="icon-small-carousel"
-									list={AlreadySeenMovieList}
-									setter={setAlreadySeenMovieList}
-									icon="bi bi-eye"
-									movie={movie}
-								/>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="movie-card-wraper">
+        <div className="movie-card">
+          <Link
+            to={`/film/${movie.id}`}
+            onClick={() => {
+              window.scrollTo({ top: 0, left: 0 });
+            }}
+          >
+            <img src={imageUrl} alt={movie.title} className="movie-image" />
+          </Link>
+          <div className="show-mini-details">
+            <p className="title-small-carousel">{movie.title}</p>
+            <div className="tag-list-carousel">
+              <Tag
+                className="icon-small-carousel"
+                list={FavoriteMoviesList}
+                setter={setFavoriteMoviesList}
+                icon="bi bi-suit-heart"
+                movie={movie}
+              />
+              <Tag
+                className="icon-small-carousel"
+                list={WatchListMovies}
+                setter={setWatchListMovies}
+                icon="bi bi-plus-circle"
+                movie={movie}
+              />
+              <Tag
+                className="icon-small-carousel"
+                list={AlreadySeenMovieList}
+                setter={setAlreadySeenMovieList}
+                icon="bi bi-eye"
+                movie={movie}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default MovieCard;
